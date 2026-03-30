8f3ebdc99baa15ef6ba933ac3bfaf9414ba1e2dc
Dhruv Sharmaa <dhruvsharma22052006@gmai.com>
2026-03-30 23:44:47 +0530

    Implement hero Section

--- Apple Design/src/App.jsx
+++ Apple Design/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import NavBar from './Component/NavBar'
+import Hero from './Component/Hero'
 const App = () => {
   return (
     <main>
         <NavBar/>
+        <Hero/>
     </main>
   )
 }

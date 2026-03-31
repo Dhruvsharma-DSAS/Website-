e2570bf26ca64f4419a6451260bec41f234ea8d9
Dhruv Sharmaa <dhruvsharma22052006@gmai.com>
2026-03-31 13:12:52 +0530

    Add 3D MacBook viewer with studio lighting setup

--- Apple Design/src/App.jsx
+++ Apple Design/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import NavBar from './Component/NavBar'
 import Hero from './Component/Hero'
+import ProductViewer from './Component/ProductViewer'
+import gsap from 'gsap' 
+import { ScrollTrigger,SplitText } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger)
+
 const App = () => {
   return (
     <main>
         <NavBar/>
         <Hero/>
+        <ProductViewer/>
     </main>
   )
 }
